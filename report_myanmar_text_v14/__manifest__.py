7a3b903d30a3574845737d32e01ac3de2d3b1764
White Star Myanmar <71964787+whitestar592020@users.noreply.github.com>
2025-02-08 15:57:32 +0630

    Add files via upload

---
+++ report_myanmar_text_v14/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Report Myanmar Text",
+    "category": "Productivity",
+    "summary": "Correct the Myanmar text on PDF reports.",
+    "author": "White Star Myanmar education center",
+    "company": "White Star Myanmar education center",
+    "maintainer": "White Star Myanmar education center",
+    "website": "https://www.facebook.com/odooerpdevelopment",
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "depends": ["base"],
+    "data": [
+        "views/report_font.xml",
+    ],
+}
